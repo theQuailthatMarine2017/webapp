5faaa3e2a8bb049355409a1f786f95bf451f55c8
RastaOnAMission <admin@admins-MacBook-Pro.local>
2019-03-10 12:28:58 +0300

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,20 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("welcome") { req in
+        return "Welome Back"
+    }
+    
+    router.post(Item.self, at: "item/create") {req, data -> String in
+        
+        return "You Posted \(data.product) \(data.price) \(data.available)"
+    }
+    
+    router.get("items") { req -> Item in
+        
+        return Item(product: "5 Inch Weave", price: 30000, available: true)
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
@@ -18,3 +32,13 @@ public func routes(_ router: Router) throws {
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
 }
+
+struct Item: Content {
+    
+    let product: String
+    let price: Int
+    let available: Bool
+    
+}
+
+
